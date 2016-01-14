c1d1048edde3dbacc9b32df1d55489be4a5992b2
Shunya Matsuno <id.shma@gmail.com>
2016-01-14 15:54:21 +0900

    OGP設定

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -18,20 +18,23 @@ router.get('/', function (req, res, next) {
     if (err) return next(err);
     res.render('index', {
       title: 'Shunya Matsuno | Web Developer',
-      articles: articles
+      articles: articles,
+      og_type: 'website'
     });
   });
 });
 
 router.get('/about', function (req, res, next) {
     res.render('about', {
-      title: 'Shunya Matsuno | About'
+      title: 'Shunya Matsuno | About',
+      og_type: 'article'
     });
 });
 
 router.get('/works', function (req, res, next) {
     res.render('works', {
-      title: 'Shunya Matsuno | Works'
+      title: 'Shunya Matsuno | Works',
+      og_type: 'article'
     });
 });
 
@@ -40,6 +43,7 @@ router.get('/works/ooparts', function (req, res, next) {
       title: 'Shunya Matsuno | Works - ooparts2015',
       works_title: 'OOPARTS GIFU 2015',
       works_img: '/img/ooparts.png',
+      og_type: 'article'
     });
 });
 
@@ -47,6 +51,7 @@ router.get('/works/sugoi_instagram', function (req, res, next) {
     res.render('sugoi_instagram', {
       title: 'Shunya Matsuno | Works - すごいインスタグラム',
       works_title: 'すごいインスタグラム',
+      og_type: 'article'
       //works_img: '/img/ooparts.png',
       //works_desc: 'aaa br'
     });
@@ -56,6 +61,7 @@ router.get('/works/korographic', function (req, res, next) {
     res.render('korographic', {
       title: 'Shunya Matsuno | Works - コログラフィック',
       works_title: 'コログラフィック',
+      og_type: 'article'
       //works_img: '/img/ooparts.png',
       //works_desc: 'aaa br'
     });
@@ -65,7 +71,8 @@ router.get('/works/shikaku', function (req, res, next) {
     res.render('shikaku', {
       title: 'Shunya Matsuno | Works - ヘイメンと９人のシカク',
       works_title: 'ヘイメンと９人のシカク',
-      works_img: '/img/shikaku_works.png'
+      works_img: '/img/shikaku_works.png',
+      og_type: 'article'
       //works_img: '/img/ooparts.png',
       //works_desc: 'aaa br'
     });
